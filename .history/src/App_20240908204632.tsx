5d094e0966c4730779d0bf0cc418b647b06657a2
nikiforovki <143290565+nikiforovki@users.noreply.github.com>
2024-09-09 19:31:10 +0300

    Release 2

--- .history/src/App_20240908204632.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MainLayout from './Companents/MainLayout';
-import './styles/GlobalStyles.css';
-
-const App = () => {
-  return <MainLayout />;
-};
-
-export default App;
